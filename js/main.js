52176873901b370f80cb5b749b9e59e273f0801a
Renzo Lombardi <rellbits@gmail.com>
2020-06-07 12:03:17 -0500

    update page animation

--- js/main.js
+++ js/main.js
@@ -28,6 +28,29 @@
 		//other options
 	});
 
+	// animate
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.ftco-animate').waypoint( function( direction ) {
+			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {			
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+					$('body .ftco-animate.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp ftco-animated');
+
+							el.removeClass('item-animate');
+						},  k * 50, 'easeInOutExpo' );
+					});
+				}, 100);	
+			}
+		} , { offset: '95%' } );
+	};
+	contentWayPoint();
+
 	// year
 	var date = new Date().getFullYear();
 	document.getElementById("year").innerHTML = date;
